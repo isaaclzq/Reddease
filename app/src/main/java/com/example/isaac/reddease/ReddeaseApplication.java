f408e1f1761554bed343b9b7b97ceef3bd9ea0aa
isaaclzq <isaac.lzy@gmail.com>
2018-01-14 14:19:27 -0800

    init

---
+++ app/src/main/java/com/example/isaac/reddease/ReddeaseApplication.java
@@ -0,0 +1,10 @@
+package com.example.isaac.reddease;
+
+import android.app.Application;
+
+/**
+ * Created by Isaac on 1/14/2018.
+ */
+
+public class ReddeaseApplication extends Application {
+}
---
+++ app/src/main/java/com/example/isaac/reddease/ui/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.isaac.reddease.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.isaac.reddease.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
